7e36762b16183d739d3d1523fb90331185b4feb5
Rany Azevedo <azevedo.rany@gmail.com>
2016-05-04 00:42:30 +1000

    changing images

--- JS/main.js
+++ JS/main.js
@@ -29,22 +29,20 @@ var text = ["Say Hello", "Manda um Oi", "Di Hola", "Dit Salut", "Ciao", "Hallo",
 
   }).on("mouseleave", function() {
     $('.aboutMeText').fadeOut('slow');
-    $('.right').css("background-image", "url(http://res.cloudinary.com/rany/image/upload/e_grayscale/v1462082611/back_fn3x3j.png)");
+    $('.right').css("background-image", "url(http://res.cloudinary.com/rany/image/upload/v1462285879/option5_zzg5iu.jpg");
   });
 
 
 //Somos Rie
   $('.somosRie').on("mouseenter", function() {
-      $('.right').css({
-        backgroundImage: "url(http://res.cloudinary.com/rany/image/upload/v1462075264/Captura_de_pantalla_2016-03-26_a_la_s_15.27.33_sbdr9e.png)",
-        backgroundSize: "cover",
-        backgroundRepeat: 'no-repeat',
-        backgroundPosition: 'center'
+    $('.right').css({
+      backgroundImage: "none",
+      backgroundColor: "white",
       });
       $('.somosRieText').fadeIn('slow');
     }).on("mouseleave", function() {
-      $('.right').css("background-image", "url(http://res.cloudinary.com/rany/image/upload/e_grayscale/v1462082611/back_fn3x3j.png)");
-      $('.somosRieText').fadeOut('slow');
+      $('.right').css("background-image", "url(http://res.cloudinary.com/rany/image/upload/v1462285879/option5_zzg5iu.jpg)");
+      $('.somosRieText').hide();
     });
 
 //Skills
@@ -55,21 +53,20 @@ var text = ["Say Hello", "Manda um Oi", "Di Hola", "Dit Salut", "Ciao", "Hallo",
 
     }).on("mouseleave", function() {
       $('.skillsText').fadeOut('slow');
-      $('.right').css("background-image", "url(http://res.cloudinary.com/rany/image/upload/e_grayscale/v1462082611/back_fn3x3j.png)");
+      $('.right').css("background-image", "url(http://res.cloudinary.com/rany/image/upload/v1462285879/option5_zzg5iu.jpg)");
     });
+
 
 //Cook App
 $('.cookrApp').on("mouseenter", function() {
-    $('.right').css({
-      backgroundImage: "url(http://res.cloudinary.com/rany/image/upload/v1462086511/ookr_wh2aa7.png)",
-      backgroundSize: "cover",
-      backgroundRepeat: 'no-repeat',
-      backgroundPosition: 'center'
+  $('.right').css({
+    backgroundImage: "none",
+    backgroundColor: "white",
     });
-    $('.cookrAppText').show();
+    $('.cookrAppText').fadeIn('slow');
   }).on("mouseleave", function() {
-    $('.right').css("background-image", "url(http://res.cloudinary.com/rany/image/upload/e_grayscale/v1462082611/back_fn3x3j.png)");
-    $('.cookrAppText').fadeOut('slow');
+    $('.right').css("background-image", "url(http://res.cloudinary.com/rany/image/upload/v1462285879/option5_zzg5iu.jpg)");
+    $('.cookrAppText').hide();
   });
 
 
